450b0bbc49af7d1f29fbddac7211d702ae8e6e0f
KevSeenan <kev.seenan@gmail.com>
2019-09-27 12:03:31 +0100

    Adds Passenger, Plane and Flight classes. Also Test files and test for getPlaneType

---
+++ src/test/java/PlaneTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class PlaneTest {
+
+    Plane plane;
+
+    @Before
+    public void setUp() {
+        plane = new Plane(PlaneType.BOEING747);
+    }
+
+    @Test
+    public void planeHasAType() {
+        //Given I have a plane
+        assertNotNull(plane);
+        //When a plane is made
+        //Then it has a type
+        assertEquals(PlaneType.BOEING747, plane.getType());
+    }
+
+}
